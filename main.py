ea928d6f1a9d67451a584a32fce8100296344446
Emanuel Cerqueira <emanuelcerqueira07@gmail.com>
2025-01-25 07:11:46 -0300

    feat: best quality

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ url = input("url >")
 yt = YouTube(url, 'WEB')
 print(yt.title)
 
-ys = yt.streams.get_highest_resolution()
+ys = yt.streams.filter(progressive=False, file_extension='mp4').order_by('resolution').desc().first()
 ys.download()
